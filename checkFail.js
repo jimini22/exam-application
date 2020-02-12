5a6a90ca6d17724c9b6832ded578850d9249f74a
jimini22 <60088737+jimini22@users.noreply.github.com>
2020-02-12 17:21:32 +0900

    3_version_0212

---
+++ checkFail.js
@@ -0,0 +1,17 @@
+function nameCheck(){
+	if (document.appForm.name.value.length==0) {
+		alert("이름을 입력하세요");
+		return;
+	}
+	
+}
+
+function ageCheck(){
+	if (appForm.age.value.length==0) {
+		alert("나이를 입력하세요");
+		
+	}
+	
+}
+
+
